1fc893557da45e7fbb2f8d9461a039c3c5aaf060
mohamed-ahmed101 <mohamed.ahmedelgawad@gmail.com>
2021-06-20 23:26:38 +0200

    add notificationStatusController

---
+++ src/controller/notificationStatusController.js
@@ -0,0 +1,33 @@
+
+const notificationStatus = require('../models/notificationStatus');
+module.exports = class notificationStatusController {
+
+    async all() {
+        return notificationStatus.find();
+    }
+
+    async filter(body) {
+        let { service_id, amount, operator_id, service_provider_id } = body;
+        return notificationStatus.find({ service_id });
+    }
+
+    async one(body) {
+        let { id: _id } = body;
+        return notificationStatus.findOne({ _id });
+    }
+
+    async save(body) {
+        const notificationsData = new notificationStatus(body)
+        return await notificationsData.insert();
+    }
+
+    async bulksave(body) {
+        return await notificationStatus.insertMany(body);
+    }
+
+    async remove(body) {
+        let userToRemove = await notificationStatus.findOne(request.params.id);
+        await notificationStatus.remove(userToRemove);
+    }
+
+}
