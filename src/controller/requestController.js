8f089d3e515be695eb1c80940740596f0bcd63cc
mohamed-ahmed101 <mohamed.ahmedelgawad@gmail.com>
2021-06-20 23:30:47 +0200

    update save request

--- src/controller/requestController.js
+++ src/controller/requestController.js
@@ -1,5 +1,11 @@
 const channelsService = require('../services/channelsService');
 const request = require('../models/request');
+const notificationStatusControllerClass = require('./notificationStatusController');
+const notificationStatusController = new notificationStatusControllerClass();
+
+const messageTempleteControllerClass = require('./messageTemplateController');
+const messageTemplateController = new messageTempleteControllerClass();
+
 module.exports = class requestController {
 
     async all() {
@@ -17,21 +23,38 @@ module.exports = class requestController {
     }
 
     async save(body) {
-        console.log("new request ->>>>>>>>>>>>>>>>>>",body)
+
+        let messageTemplateResult = await messageTemplateController.one({ id: body.messageID });
+        if (!messageTemplateResult) throw { serverCode: 404, serverMsg: "messageID not exist enter vaild one" }
+        let channelsServiceCheck = await channelsService.check(body.channel, body);
+        channelsServiceCheck?.serverCode && (body.status = channelsServiceCheck.serverMsg);
         const notificationReq = new request(body);
-        let save= await notificationReq.save();
-        let res = await channelsService.process(notificationReq.category, notificationReq);
-        //res channel not exit save fail
-        //process >> save 
-        //tODO add raw for each mesage per user pending//processing
-        
-        //id fail ,id success
-        return {save , res };
+        let saveRequestResult = await notificationReq.save();
+        if (body.status) throw channelsServiceResult;
+        let channelsServiceResult = await channelsService.process(body.channel, {...body, content: messageTemplateResult.content }, saveRequestResult._id);
+        if(channelsServiceResult?.serverCode) throw channelsServiceResult;
+        let notificationsStatusData = this.requestFormat(body, saveRequestResult._id);
+        await notificationStatusController.bulksave(notificationsStatusData);
+        return saveRequestResult;
     }
 
     async remove(body) {
         let userToRemove = await request.findOne(request.params.id);
         await request.remove(userToRemove);
+    }
+
+    requestFormat(data, requestId) {
+
+        let { usersList, channel } = data
+        return usersList.map(({ userIdentifier }) => {
+            return {
+                userIdentifier,
+                requestId,
+                channel,
+                status: { code: "inprogress" }
+            }
+        })
+
     }
 
 }
