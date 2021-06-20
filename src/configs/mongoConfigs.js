9b823a7685da8f9b44589a946cf6a76ea112e3cc
mohamed-ahmed101 <mohamed.ahmedelgawad@gmail.com>
2021-06-20 23:24:20 +0200

    update mongoConfigs

--- src/configs/mongoConfigs.js
+++ src/configs/mongoConfigs.js
@@ -1,3 +1,11 @@
-module.exports={
-    uri : "mongodb://localhost:27017/notifications"
-}
+module.exports = {
+    development: {
+        uri : "mongodb://localhost:27017/notifications"
+    },
+    UAT: {
+        uri : "mongodb://localhost:27017/notifications"
+    },
+    production: {
+        uri : "mongodb://localhost:27017/notifications"
+    }
+  }[process.env.NODE_ENV || 'development']
