29f190f90b1ff8152769202e670cd1bdb551b460
mohamed-ahmed101 <mohamed.ahmedelgawad@gmail.com>
2021-06-20 23:29:49 +0200

    update pushNotification with queueLimit configs and add some logic

--- src/services/pushNotificationService.js
+++ src/services/pushNotificationService.js
@@ -1,13 +1,32 @@
 const channelClass = require('../classes/channel');
+const logger = require('../logger');
+const redisConfig = require('../configs/redisConfigs');
 const configs = {
-    name : "pushNotificationService"
+    name: "pushNotificationService",
+    serviceQueue: "PUSHQueue",
+    queueLimit : 50,
+    limitInterval : 60000,
+    redisConfig
 }
- class pushNotificationService extends channelClass {
+class pushNotificationService extends channelClass {
 
     constructor() {
-        super(configs)
+        super(configs);
+        this.queue.onMessage = this.process.bind(this);
     }
 
- }
+    async process(data) {
+        logger.info(`new rconsumedddequest consumed at ${configs.name} `, { data });
+        this.send(data)
+        return { message: "your request has been processed" };
+    }
 
- module.exports = pushNotificationService;
+    async send(msg) {//specified for each channel             
+            //simulate send
+            console.log("msg send ", JSON.parse(msg))
+          //  this.notify(msg, Math.round(Math.random()))
+
+    }
+}
+
+new pushNotificationService();
