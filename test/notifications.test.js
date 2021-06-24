485cfb8b64aa2b6dc0ad8b30c6962d84125a4994
alaahayba <alaahyba14@gmail.com>
2021-06-24 20:08:10 +0200

    add testing packages

--- test/notifications.test.js
+++ test/notifications.test.js
@@ -1,37 +1,58 @@
-const chai = require('chai');
-const expect = chai.expect;
 const app = require('../src/server');
-process.env.PORT = "4001";
-var chai = require('chai'),
-chaiHttp = require('chai-http');
+var chai = require('chai'), chaiHttp = require('chai-http');
+const expect = chai.expect;
 chai.use(chaiHttp);
-
-chai.use(chaiHttp);
-let requestData = {
+let requestData =
+{
     "usersList": [{
         "userIdentifier": "01015842240",
         "preferredLang": "arrLang",
         "templateData": {
-            "promoCode": "428555"
+            "name": "mohamed"
+        }
+    },
+    {
+        "userIdentifier": "01015842240",
+        "preferredLang": "engLang",
+        "templateData": {
+            "name": "ahmed"
         }
     }],
-    "messageID": "60d24cce9b0fb600230730ca",
+    "messageID": "60d49fd758172b23fe386a30",
     "channel": "PUSH",
     "expireAt": 1999314790830,
     "sendAt": 0,
     "retry": 2
 }
 
-describe('POST /addRequest', () => {
+describe('POST /addRequest test', () => {
     it('should return the added /addRequest', done => {
         chai
-            .request(app)
-            .POST('/addRequest')
-            .body(requestData)
+            .request('http://localhost:3000')
+            .post('/user/me')
+            .send({ password: '123', confirmPassword: '123' })
             .end((err, res) => {
-                res.should.have.status(200);
-                console.log(res.body)
-                //   expect(res.body).to.;
+                console.log(err, res.body)
+                console.log(res.statusCode)
+                expect(res.statusCode).to.equal(404);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.property('Error');
+                done();
+            });
+    });
+
+    it('should return the added /addRequest', done => {
+        chai
+            .request('http://localhost:3000')
+            .post('/addRequest')
+            .send(requestData)
+            .end((err, res) => {
+                console.log(err, res.body)
+                console.log(res.statusCode)
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body).not.to.have.property('Error');
+
                 done();
             });
     });
